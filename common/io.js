ba33df09fd7f062f0dfd74e4d9ae17e7d4ae9972
Wyatt Howe <wyatt@wyatthowe.net>
2020-08-11 14:30:23 -0400

    Initiate Firebase from secret url parameter

--- common/io.js
+++ common/io.js
@@ -1,10 +1,42 @@
-firebaseConfig = {};
-
 __collection = null;
 
 setTimeout(function () {
-    firebase.initializeApp(self.firebaseConfig);
-    __collection = firebase.database().ref().child('channel-private-id');
+  sodium.ready.then(function () {
+    const __key = window.location.search.substring(1).split('&').map(e => e.split('=')).filter(e => e[0] === 'access_key')[0][1];
+    const __storage = [
+      'I8xyy-YSHleWq-RfrE7aq5_dDkjcqxQa6vxnnh1BJIguU9qCVcQIsg' +
+      '_-e7wXaVtj_jMpzMvOUhWHAKkyiImKEBNXXRLYTLnMuY9TTMv2Yk2U' +
+      'u1kMt-dmGXOhVMBIugSHKfJXHGt70Kc5lueVqi37S5uubybg8RZBON' +
+      'bV5STRpp54ec1Cq0eMveW-hKZjyHFoQh-pVk44_BqYLaDQrHOg1qTq' +
+      'KSTpC3mUSp9mPXy-2CZbq0NZernsWKKgMH-4430inHFpKhJrLOPAcg' +
+      'MZOjl-cr88uipZFTDopjZJPFxeV6WTykzBEFyr7RNkFr7Q3UpCxj0l' +
+      'hs-JWgXSo1Uwrjicf6ZdC1baihbL_QR3qtzGC7_AF4yuCl8ymFgr0G' +
+      '7lNzrIncHvPJug2oJMDbjMJm4Y3jqImVXJJGmX1yHSGUFxxtOvQ68h' +
+      'vcX2DoQSanFNNZuMTA8HuUaKQB0RYpdncTlgmho', 'hHPj5xRKAvr' +
+      'SuuhtwIGJ9aXdQBKmRs_eT8'
+    ];
+
+    try {
+      const firebaseConfig = JSON.parse(
+        sodium.to_string(
+          sodium.crypto_aead_chacha20poly1305_decrypt(null,
+            sodium.from_base64(__storage[0]), null, new Uint8Array(8),
+            sodium.from_base64(__key + __storage[1])
+          )
+        )
+      );
+
+      firebase.initializeApp(firebaseConfig);
+      __collection = firebase.database().ref().child('channel-private-id');
+
+      $('#ContributeButton').prop('disabled', false);
+    } catch (e) {
+      if (e.toString() === 'Error: invalid input') {
+        e = new Error('Failed to authenticate firebase for communications.  Bad API key?');
+      }
+      throw e;
+    }
+  });
 }, 1000);
 
 const compress = m => m.split('|').map(x => JSON.parse(x).gz.map(n => (n).toString(36).padStart(2, '0')).join('')).join('|');
@@ -15,11 +47,6 @@ function give(tag, msg, json_gz = true, overwrite = true) {
     if (json_gz) {
         msg = compress(msg);
     }
-    // if (overwrite) {
-    //     clear(tag).then(function () {
-    //         __collection.push().set({tag, msg})
-    //     });
-    // }
     return __collection.push().set({tag, msg});
 }
 
